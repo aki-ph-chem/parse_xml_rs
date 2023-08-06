87aba6bfecaef3e4b9a627b52235b4632326addb
aki <aki@mymail.com>
2023-08-06 23:14:47 +0900

    AsymmetricManifold, Parameterを取り出せるようにした

--- parse_pgo_file/src/consts.rs
+++ parse_pgo_file/src/consts.rs
@@ -2,13 +2,17 @@ use xmltree::Element;
 use std::{error, fs};
 
 struct Traverse<'a> {
-    target: &'a str,
+    target_1: &'a str,
+    target_2: &'a str,
 }
 
 impl Traverse<'_> {
 
     fn traverse(&self, element: xmltree::Element, e_list: &mut Vec<xmltree::Element>) {
-        if element.name == self.target  {
+        if element.name == self.target_1  {
+            e_list.push(element.clone());
+            return;
+        } else if element.name == self.target_2 {
             e_list.push(element.clone());
             return;
         } 
@@ -27,8 +31,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let pgo_file = fs::File::open(path)?;
     let elements = Element::parse(pgo_file)?;
 
-    //let tr = Traverse{target: "AsymmetricTop"};
-    let tr = Traverse{target: "AsymmetricManifold"};
+    let tr = Traverse{
+        target_1: "AsymmetricManifold",
+        target_2: "Parameter",
+    };
 
     let mut mol = vec![]; 
     tr.traverse(elements, &mut mol);
